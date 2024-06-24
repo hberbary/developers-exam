331364155e4440d2799c5270d48c54da3d3d965d
Hermmann Berbary <h.berbary@gmail.com>
2024-06-24 09:46:55 -0300

    added error page and feedback page

--- src/WebApp/Controllers/HomeController.cs
+++ src/WebApp/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ public class HomeController : Controller
     public IActionResult Index()
         => View();
 
-    public IActionResult Privacy()
+    public IActionResult Feedback()
         => View();
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
